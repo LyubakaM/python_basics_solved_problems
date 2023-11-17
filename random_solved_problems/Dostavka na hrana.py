f13024686d2b0f7c74cb89330b7e68e5a947545c
Lyuben Murtev <143640789+LyubakaM@users.noreply.github.com>
2023-11-17 13:55:19 +0200

    Add files via upload

---
+++ random_solved_problems/Dostavka na hrana.py
@@ -0,0 +1,14 @@
+pilem = int(input())
+ribam = int(input())
+vegm = int(input())
+
+p = 10.35
+r = 12.40
+v = 8.15
+sump = pilem * p
+sumr = ribam * r
+sumv = vegm * v
+sum = sump + sumr + sumv
+desert = sum * 0.2
+all = sum + desert + 2.50
+print(all)
